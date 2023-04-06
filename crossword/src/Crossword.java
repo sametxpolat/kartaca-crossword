cb8dc216eb27b366dec88d9def22b754699d54ad
Samet POLAT <sametxpolat@gmail.com>
2023-04-06 07:46:52 +0300

    add inputs type

--- crossword/src/Crossword.java
+++ crossword/src/Crossword.java
@@ -1,7 +1,19 @@
 public class Crossword {
+    
+    /*
+     * 
+     * task:
+     *  use: a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+     *   input: ab, z
+     *   result: zy, a
+     * 
+     */
+
+    public static String alphapet = "";
+
     public static void main(String[] args) throws Exception {
         
-        
+        String[] inputs = { };
 
     }
 }
